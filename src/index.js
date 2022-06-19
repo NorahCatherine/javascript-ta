780c1b392f33100e22b7de5008bc77eec3bb86e8
NorahCatherine <norahcatherinenansubuga@gmail.com>
2022-06-19 14:36:25 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 // complete the function
 function prime (num) {
   // code goes here
+  const number = parseInt(prompt("Enter a positive number: "));
+  let isPrime = true;
+  
+ 
+  if (number === 1) {
+      console.log("1 is neither prime nor composite number.");
+  }
+  
+  
+  else if (number > 1) {
+  
+    
+      for (let i = 2; i < number; i++) {
+          if (number % i == 0) {
+              isPrime = false;
+              break;
+          }
+      }
+  
+      if (isPrime) {
+          console.log(`${number} is a prime number`);
+      } else {
+          console.log(`${number} is a not prime number`);
+      }
+  }
+  
+ 
+  else {
+      console.log("The number is not a prime number.");
+  }
+
 }
 
 function solution (arg) {
